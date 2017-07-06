6ce9441e249ba9f8ae051429dfcb74307fd5a260
Maarten Hus <maarten.hus@42.nl>
2017-07-06 10:00:17 +0200

    Created a small program to sort yml files alphabetically.

---
+++ README.md
@@ -0,0 +1,28 @@
+# About
+
+This is a tool which allows you to sort yml files via a CLI.
+
+The tool is basically a tiny wrapper around the great [js-yaml](https://github.com/nodeca/js-yaml).
+
+# Installation
+
+`npm install -g yml-sorter`
+
+# Usage
+
+```
+Usage: yml-sorter [options]
+
+Options:
+  --input, -i    The yml file which needs to be sorted  [required]
+  --output, -o   The file to wich to write the output
+  --dry-run, -d  Only outputs the proposed sort to the terminal  [default: false]
+  -h, --help     Show help  [boolean]
+
+Examples:
+  yml-sorter --input application-yml                       Sorts the file application.yml alphabetically.
+  yml-sorter --input application-yml --output dragons.yml  Sorts and writes the output to dragons.yml
+  yml-sorter --input application-yml -dry-run              Writes the output to the terminal
+
+With love from 42.nl
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+const argv = require("yargs")
+  .usage("Usage: yml-sorter [options]")
+  .example(
+    "yml-sorter --input application-yml",
+    "Sorts the file application.yml alphabetically."
+  )
+  .example(
+    "yml-sorter --input application-yml --output dragons.yml",
+    "Sorts and writes the output to dragons.yml"
+  )
+  .example(
+    "yml-sorter --input application-yml -dry-run",
+    "Writes the output to the terminal"
+  )
+  .describe("input", "The yml file which needs to be sorted")
+  .option("input", {
+    alias: "i",
+  })
+  .describe("output", "The file to wich to write the output")
+  .option("output", {
+    alias: "o",
+  })
+  .describe("dry-run", "Only outputs the proposed sort to the terminal")
+  .option("dry-run", {
+    alias: "d",
+    default: false
+  })
+  .demandOption(["input"])
+  .help("h")
+  .alias("h", "help")
+  .epilog("With love from 42.nl")
+  .wrap(null).argv;
+
+yaml = require("js-yaml");
+fs = require("fs");
+
+// Get document, or throw exception on error
+try {
+  const doc = yaml.safeLoad(fs.readFileSync(argv.input, "utf8"));
+
+  const input = yaml.safeDump(doc, { sortKeys: true });
+
+  if (argv["dry-run"]) {
+    console.log(input);
+  } else {
+    const output = argv.output === false ? argv.input : argv.output;
+
+    fs.writeFile(output, input, error => {
+      if (error) {
+        return console.error(errore);
+      }
+    });
+  }
+} catch (error) {
+  console.error(error);
+}
